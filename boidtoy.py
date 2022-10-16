eff7a2131a72d8862d65ad5e81411c4aa136e492
HaylockGrant <HaylockGrant@Gmail.com>
2022-10-16 22:01:31 +0100

    commit on main

--- boidtoy.py
+++ boidtoy.py
@@ -4,6 +4,7 @@ import random
 import math
 from ciede2000 import *
 from colorMan import *
+from physicsMan import *
 
 
 #initialize the screen and display
@@ -41,15 +42,3 @@ while running:
     if(doPhysics() or redraw): #the physics loop returns true if there was an update to the screen
         doDraw()
     clock.tick(30)
-
-
-#physics loop
-def doPhysics():
-    return True
-    #this function will calculate the physics of the boids
-
-#draw loop, this class will draw the boids to the screen and update the screen. it may not always be necessary to update the screen if the physics loop did not change anything
-def doDraw():
-    pygame.display.update()
-    #this function will draw the boids to the screen after the physics loop
-
